e941c39df00d4d644baf7be347d80c21064ce1b1
Anil Kumar <anilsingh.singh09@gmail.com>
2026-07-13 21:02:01 +0530

    feat: restructure app layout with sidebar and mobile menu
    - Updated app.html to implement a sidebar navigation with collapsible functionality.
    - Added mobile menu toggle for better responsiveness on smaller screens.
    - Integrated toast notifications using PrimeNG for user feedback.
    - Enhanced app routing to include quiz and dashboard components.
    - Refactored app.ts to manage sidebar state and mobile menu visibility.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'quiz',
+        loadComponent: () => import('./feature/quiz-view/quiz-view').then(m => m.QuizView)
+    },
+    {
+        path: '',
+        loadComponent: () => import('./feature/dashboard/dashboard').then(m => m.Dashboard)
+
+    }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,32 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit, signal } from '@angular/core';
+import { RouterOutlet, RouterLinkWithHref, RouterLinkActive } from '@angular/router';
+import { MenuItem, MessageService } from 'primeng/api';
+import { ToastModule } from 'primeng/toast';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ToastModule, RouterLinkWithHref, RouterLinkActive],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit {
+
+  readonly mobileMenuOpen = signal(false);
   protected readonly title = signal('mordenec');
+  readonly sidebarCollapsed = signal(true);
+  constructor(private messageService: MessageService) { }
+  ngOnInit(): void {
+  }
+  toggleSidebar(): void {
+    if (typeof window !== 'undefined' && window.innerWidth < 1024) {
+      this.mobileMenuOpen.set(!this.mobileMenuOpen());
+      return;
+    }
+
+    this.sidebarCollapsed.set(!this.sidebarCollapsed());
+  }
+
+  closeSidebar(): void {
+    this.mobileMenuOpen.set(false);
+  }
 }
