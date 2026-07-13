e31cb28fd08297514c259f25b0517539e9f1eb0c
Anil Kumar <anilsingh.singh09@gmail.com>
2026-07-13 21:00:17 +0530

    Update dependencies and enhance application configuration
    - Added new dependencies: @primeng/themes, chart.js, ngx-highlightjs, postcss, primeicons, primeng, tailwindcss, and tailwindcss-primeui.
    - Updated existing dependencies: @tailwindcss/postcss.
    - Enhanced app configuration with PrimeNG and highlight.js integration.
    - Improved global styles in styles.css for better theming and layout.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,39 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import { MessageService } from 'primeng/api';
+import Aura from '@primeng/themes/aura';
 import { routes } from './app.routes';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { globalErrorInterceptor } from './core/interceptors/global-error-interceptor';
+import { provideHighlightOptions } from 'ngx-highlightjs';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideAnimationsAsync(),
+    provideRouter(routes),
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+        options: {
+          darkModeSelector: false || 'none',
+          cssLayer: {
+            name: 'primeng',
+            order: 'primeng tailwind',
+          },
+        }
+      }
+    }),
+    provideHttpClient(
+      withInterceptors([
+        globalErrorInterceptor
+      ])
+    ),
+    provideHighlightOptions({
+      fullLibraryLoader: () => import('highlight.js')
+    }),
+    MessageService
   ]
 };
