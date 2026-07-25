bc0bbe6cf035021d345910265e4fb8729ac47fbe
Anil Kumar <anilsingh.singh09@gmail.com>
2026-07-25 14:42:02 +0530

    update path and remove unused code

--- src/app/core/services/apis/ai-api.service.ts
+++ src/app/core/services/apis/ai-api.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Service } from '@angular/core';
-import { environment } from '../../../environments/environment.development';
+import { environment } from '../../../environments/environment';
 import { Observable, of } from 'rxjs';
 import { InterviewQuestion } from '../../models/interview-question.model';
 import { AIEvaluationResult } from '../ai-question';
@@ -25,22 +25,6 @@ export class AiApiService {
     userRole: string,
     experienceLevel: string,
   ): Observable<InterviewQuestion[]> {
-    return of([
-      {
-        question:
-          'Explain the concept of dependency injection in Java Spring Boot and how it enhances the maintainability of the application.',
-        type: 'technical',
-        skill: 'Java Spring Boot',
-        level: 'intermediate',
-      },
-      {
-        question:
-          'Can you describe a situation where you had to provide technical direction to a development team? How did you ensure the team adhered to architectural guidelines and best practices?',
-        type: 'behavioral',
-        skill: 'Leadership',
-        level: 'advanced',
-      },
-    ]);
     return this.http.post<InterviewQuestion[]>(`${this.apiUrl}/ai-question-sets/from-topic`, {
       topic,
       userRole,
@@ -53,85 +37,6 @@ export class AiApiService {
     userRole: string,
     experienceLevel: string,
   ): Observable<InterviewQuestion[]> {
-    return of([
-      {
-        question:
-          'How would you approach designing a scalable system landscape for currency adaptation in banknote processing machines, considering factors like security, maintainability, and performance?',
-        type: 'scenario',
-        skill: 'System Design',
-        level: 'advanced',
-      },
-      {
-        question:
-          'Explain the concept of dependency injection in Java Spring Boot and how it enhances the maintainability of the application.',
-        type: 'technical',
-        skill: 'Java Spring Boot',
-        level: 'intermediate',
-      },
-      {
-        question:
-          'Can you describe a situation where you had to provide technical direction to a development team? How did you ensure the team adhered to architectural guidelines and best practices?',
-        type: 'behavioral',
-        skill: 'Leadership',
-        level: 'advanced',
-      },
-      {
-        question:
-          'How do you optimize the performance of a Docker container in a Microsoft Azure environment, and what tools or methods do you use for monitoring?',
-        type: 'technical',
-        skill: 'Docker and Azure',
-        level: 'intermediate',
-      },
-      {
-        question:
-          'Tell me about a time when you had to troubleshoot a complex technical issue in a production environment. How did you resolve it, and what did you learn from the experience?',
-        type: 'behavioral',
-        skill: 'Problem Solving',
-        level: 'advanced',
-      },
-      {
-        question:
-          'Explain the differences between RDBMS and NoSQL databases, and how you would decide which to use in a given scenario.',
-        type: 'technical',
-        skill: 'Database Systems',
-        level: 'intermediate',
-      },
-      {
-        question:
-          'Describe your experience with Agile methodologies and how you contribute to Agile ceremonies. How do you ensure that technical documentation is up-to-date and relevant?',
-        type: 'behavioral',
-        skill: 'Agile Methodologies',
-        level: 'intermediate',
-      },
-      {
-        question:
-          'How do you approach the design of a full-stack application, integrating backend, frontend, and relational database systems? What considerations do you take into account for scalability and security?',
-        type: 'technical',
-        skill: 'Full-stack Development',
-        level: 'advanced',
-      },
-      {
-        question:
-          'Can you walk me through your process for conducting a code review, including what aspects you focus on and how you provide constructive feedback to the development team?',
-        type: 'behavioral',
-        skill: 'Code Review',
-        level: 'intermediate',
-      },
-      {
-        question:
-          'Explain how you would guide the implementation of a new technology, such as image processing or machine learning capabilities, within an existing application platform.',
-        type: 'scenario',
-        skill: 'Technical Innovation',
-        level: 'advanced',
-      },
-      {
-        question:
-          'Describe your experience with Angular and how you would lead the design and implementation of Angular components within a larger application framework.',
-        type: 'technical',
-        skill: 'Angular',
-        level: 'intermediate',
-      },
-    ]);
     return this.http.post<InterviewQuestion[]>(
       `${this.apiUrl}/ai-question-sets/from-job-description`,
       {
@@ -143,22 +48,6 @@ export class AiApiService {
   }
 
   getAiGeneratedQuestion(type: string, level: string): Observable<InterviewQuestion[]> {
-    return of([
-      {
-        question:
-          'Explain the concept of dependency injection in Java Spring Boot and how it enhances the maintainability of the application.',
-        type: 'technical',
-        skill: 'Java Spring Boot',
-        level: 'intermediate',
-      },
-      {
-        question:
-          'Can you describe a situation where you had to provide technical direction to a development team? How did you ensure the team adhered to architectural guidelines and best practices?',
-        type: 'behavioral',
-        skill: 'Leadership',
-        level: 'advanced',
-      },
-    ]);
     return this.http.get<InterviewQuestion[]>(`${this.apiUrl}/ai-questions`, {
       params: { type, level },
     });
@@ -168,10 +57,5 @@ export class AiApiService {
 interface ApiEvalBody {
   question: string;
   answer: string;
-  weight?: number|string;
+  weight?: number | string;
 }
-
-// app.use("/api/ai-questions", aiQuestionRoutes);
-// app.use("/api/ai", aiRoutes);
-// app.use("/api/ai-evaluations", aiEvaluationRoutes);
-// app.use("/api/ai-question-sets", aiQuestionSetRoutes);
